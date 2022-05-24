4f7fdb3a6ea6651c663cca8d1a583aa2fbb70e08
N_20 <N_20@DESKTOP-GTN3FRP>
2022-05-24 08:08:42 +0400

    Добавьте файлы проекта.

---
+++ LR3_ADS_2.0.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+class Graph {
+	size_t count;
+	vector<list<TVertex>> adj;
+public:
+	Graph() {
+		count = 0;
+	}
+};
+
+class locality {
+	string name;
+	int population;
+
+public:
+	locality() {
+		name = "unknown";
+		population = 0;
+	}
+};
+
+struct TVertex {
+	locality a;
+	TVertex* next;
+	TEdge* c;
+
+	TVertex() {
+		locality();
+		next = nullptr;
+		c = nullptr;
+	}
+};
+
+struct TEdge {
+	double length;
+	bool type;// 0 - асфальт, 1 - грунт;
+	bool payment;
+};
+
+

e1a2ab520144655a8765ef8786e953fb18ccac79
N_20 <N_20@DESKTOP-GTN3FRP>
2022-05-30 13:33:21 +0400

    добавлены номера вершин графа

--- LR3_ADS_2.0.cpp
+++ LR3_ADS_2.0.cpp
@@ -41,18 +41,19 @@ struct Edge {           // ребро - путь/дорога
 };
 
 struct Vertex { // вершина - населенный пункт
-	//size_t number;
 	string name;
 	size_t population;
 	
 	list<Edge> edgelist;
+    int number;
 
-	Vertex(): name(""),population(0){}
-	Vertex(const string& _name, const size_t _population) : name(_name), population(_population) {}
+	Vertex(): name(""),population(0), number(-1) {}
+	Vertex(const string& _name, const size_t _population) : name(_name), population(_population), number(-1) {}
 
 	Vertex(const Vertex& temp){
 		name = temp.name;
 		population = temp.population;
+        number = temp.number;
 	}
 
 	~Vertex() {}
@@ -65,6 +66,9 @@ struct Vertex { // вершина - населенный пункт
 
     void set(string _name, size_t _population) { name = _name; population = _population; }
 	list<Edge> get_edge_list() const { return edgelist; }
+
+    int get_number() const { return number; }
+    void set_number(int _number) { number = _number; }
 
 	void print_edge_list() {
 		cout << "[";
@@ -84,6 +88,7 @@ struct Vertex { // вершина - населенный пункт
 	Vertex& operator=(const Vertex& temp){
 		if (this == &temp) return *this;
 		name = temp.name;
+        number = temp.number;
 		population = temp.population;
 		return *this;
 	}
@@ -93,7 +98,7 @@ struct Vertex { // вершина - населенный пункт
 
 class Graph {
 	vector<Vertex> vertices;
-	size_t count;
+	int count;
 public:
 	Graph(): count(0) {}
 	 ~Graph() {}
@@ -133,22 +138,24 @@ public:
 
      void add_vertex(Vertex temp) {
          bool check = check_if_vertex_exist_by_name(temp.get_name());
-         if (check == true) {
+         if (check) {
              cout << "Vertex with this name already exist" << endl;
          }
          else {
+             temp.set_number(count);
              vertices.push_back(temp);
+             count++;
              cout << "New Vertex Added Successfully" << endl;
          }
      }
   
-     void add_edge_between_vertices(string fromVertex, string toVertex, size_t _population, bool t,bool p) {
+     void add_edge_between_vertices(string fromVertex, string toVertex, size_t _length, bool t,bool p) {
          bool check1 = check_if_vertex_exist_by_name(fromVertex);
          bool check2 = check_if_vertex_exist_by_name(toVertex);
 
          bool check3 = check_if_edge_exist_between_vertices(fromVertex, toVertex);
          if (check1 && check2) {
-             if (check3 == true) {
+             if (check3) {
                  cout << "Edge between " << get_vertex_by_name(fromVertex).get_name() << "(" << fromVertex << ") and " << get_vertex_by_name(toVertex).get_name() << "(" << toVertex << ") already exist" << endl;
              }
              else {
@@ -156,13 +163,14 @@ public:
                  for (int i = 0; i < vertices.size(); i++) {
 
                      if (vertices.at(i).get_name() == fromVertex) {
-                         Edge e(toVertex, _population, t, p);
+                         Edge e(toVertex, _length, t, p);
                          vertices.at(i).edgelist.push_back(e);
+
                      }
-                     else if (vertices.at(i).get_name() == toVertex) {
-                         Edge e(toVertex, _population, t, p);
+                     /*else if (vertices.at(i).get_name() == toVertex) {
+                         Edge e(toVertex, _length, t, p);
                          vertices.at(i).edgelist.push_back(e);
-                     }
+                     }*/
                  }
 
                  cout << "Edge between " << fromVertex << " and " << toVertex << " added successfully" << endl;
@@ -227,7 +235,7 @@ public:
              if (vertices.at(i).get_name() == vid) {
                  cout << "[";
                  for (auto it = vertices.at(i).edgelist.begin(); it != vertices.at(i).edgelist.end(); it++) {
-                     cout << it->get_destination();
+                     cout << it->get_destination()<<", ";
                  }
                  cout << "]";
              }
@@ -235,11 +243,18 @@ public:
 
      }
 
+     void re_count() {
+         for (size_t i = 0; i < vertices.size(); i++) {
+             vertices.at(i).set_number(i);
+             count = i;
+         }
+     }
+
      void print() {
          for (size_t i = 0; i < vertices.size(); i++) {
              Vertex temp;
              temp = vertices.at(i);
-             cout << temp.get_name() << " (" << temp.get_people_count() << ") --> ";
+             cout << temp.get_number()<< temp.get_name() << " (" << temp.get_people_count() << ") --> ";
              temp.print_edge_list();
          }
      }
@@ -247,116 +262,133 @@ public:
 
 int main() {
 Graph G1;
-size_t option;
+Vertex v1,v2,v3,v4;
+
+v1.set("A", 1);
+G1.add_vertex(v1);
+
+v2.set("B", 2);
+G1.add_vertex(v2);
+
+v3.set("C", 3);
+G1.add_vertex(v3);
+
+v4.set("D", 4);
+G1.add_vertex(v4);
+
+G1.print();
+
+
+size_t option=0;
 
 string _name,_name2;
 int _population,  w;
 bool t = 1, p = 0;
 bool check;
 
-do {
-    cout << "What operation do you want to perform? " <<
-        " Select Option number. Enter 0 to exit." << endl;
-    cout << "1. Add Vertex" << endl;
-    cout << "2. Update Vertex" << endl;
-    cout << "3. Delete Vertex" << endl;
-    cout << "4. Add Edge" << endl;
-    cout << "5. Update Edge" << endl;
-    cout << "6. Delete Edge" << endl;
-    cout << "7. Check if 2 Vertices are Neigbors" << endl;
-    cout << "8. Print All Neigbors of a Vertex" << endl;
-    cout << "9. Print Graph" << endl;
-    cout << "10. Clear Screen" << endl;
-    cout << "0. Exit Program" << endl;
-
-    cin >> option;
-    Vertex v1;
-
-    switch (option) {
-    case 0: break;
-
-    case 1:
-        cout << "Add vertex: \n";
-        cout << "Enter state name :";
-        cin >> _name;
-        cout << "Enter number of population :";
-        cin >> _population;
-
-        v1.set(_name, _population);
-        G1.add_vertex(v1);
-
-        break;
-
-    case 2:
-       
-    case 3:
-        cout << "Delete Vertex Operation -" << endl;
-        cout << "Enter ID of Vertex(State) to Delete : ";
-        cin >> _name;
-        G1.deleteVertexByID(_name);
-
-        break;
-
-    case 4:
-        cout << "Add Edge Operation -" << endl;
-        cout << "Enter ID of Source Vertex(State): ";
-        cin >> _name;
-        cout << "Enter ID of Destination Vertex(State): ";
-        cin >> _name2;
-        cout << "Enter Weight of Edge: ";
-        cin >> w;
-        G1.add_edge_between_vertices(_name, _name2, w,t,p);
-
-        break;
-
-    case 5:
-
-    case 6:
-        cout << "Delete Edge Operation -" << endl;
-        cout << "Enter ID of Source Vertex(State): ";
-        cin >> _name;
-        cout << "Enter ID of Destination Vertex(State): ";
-        cin >> _name2;
-        G1.deleteEdgeByID(_name, _name2);
-
-        break;
-
-    case 7:
-        cout << "Check if 2 Vertices are Neigbors -" << endl;
-        cout << "Enter ID of Source Vertex(State): ";
-        cin >> _name;
-        cout << "Enter ID of Destination Vertex(State): ";
-        cin >> _name2;
-        check = G1.check_if_edge_exist_between_vertices(_name, _name2);
-        if (check == true) {
-            cout << "Vertices are Neigbors (Edge exist)";
-        }
-        else {
-            cout << "Vertices are NOT Neigbors (Edge does NOT exist)";
-        }
-
-        break;
-
-    case 8:
-        cout << "Print All Neigbors of a Vertex -" << endl;
-        cout << "Enter ID of Vertex(State) to fetch all Neigbors : ";
-        cin >> _name;
-        G1.getAllNeigborsByID(_name);
-
-        break;
-
-    case 9:
-        cout << "Print Graph Operation -" << endl;
-        G1.print();
-
-        break;
-
-    default:
-        cout << "Enter Proper Option number " << endl;
-    }
-    cout << endl;
-
-} while (option != 0);
+//do {
+//    cout << "What operation do you want to perform? " <<
+//        " Select Option number. Enter 0 to exit." << endl;
+//    cout << "1. Add Vertex" << endl;
+//    cout << "2. Update Vertex" << endl;
+//    cout << "3. Delete Vertex" << endl;
+//    cout << "4. Add Edge" << endl;
+//    cout << "5. Update Edge" << endl;
+//    cout << "6. Delete Edge" << endl;
+//    cout << "7. Check if 2 Vertices are Neigbors" << endl;
+//    cout << "8. Print All Neigbors of a Vertex" << endl;
+//    cout << "9. Print Graph" << endl;
+//    cout << "10. Clear Screen" << endl;
+//    cout << "0. Exit Program" << endl;
+//
+//    cin >> option;
+//    Vertex v1;
+//
+//    switch (option) {
+//    case 0: break;
+//
+//    case 1:
+//        cout << "Add vertex: \n";
+//        cout << "Enter state name :";
+//        cin >> _name;
+//        cout << "Enter number of population :";
+//        cin >> _population;
+//
+//        v1.set(_name, _population);
+//        G1.add_vertex(v1);
+//
+//        break;
+//
+//    case 2:
+//       
+//    case 3:
+//        cout << "Delete Vertex Operation -" << endl;
+//        cout << "Enter ID of Vertex(State) to Delete : ";
+//        cin >> _name;
+//        G1.deleteVertexByID(_name);
+//
+//        break;
+//
+//    case 4:
+//        cout << "Add Edge Operation -" << endl;
+//        cout << "Enter ID of Source Vertex(State): ";
+//        cin >> _name;
+//        cout << "Enter ID of Destination Vertex(State): ";
+//        cin >> _name2;
+//        cout << "Enter Weight of Edge: ";
+//        cin >> w;
+//        G1.add_edge_between_vertices(_name, _name2, w,t,p);
+//
+//        break;
+//
+//    case 5:
+//
+//    case 6:
+//        cout << "Delete Edge Operation -" << endl;
+//        cout << "Enter ID of Source Vertex(State): ";
+//        cin >> _name;
+//        cout << "Enter ID of Destination Vertex(State): ";
+//        cin >> _name2;
+//        G1.deleteEdgeByID(_name, _name2);
+//
+//        break;
+//
+//    case 7:
+//        cout << "Check if 2 Vertices are Neigbors -" << endl;
+//        cout << "Enter ID of Source Vertex(State): ";
+//        cin >> _name;
+//        cout << "Enter ID of Destination Vertex(State): ";
+//        cin >> _name2;
+//        check = G1.check_if_edge_exist_between_vertices(_name, _name2);
+//        if (check == true) {
+//            cout << "Vertices are Neigbors (Edge exist)";
+//        }
+//        else {
+//            cout << "Vertices are NOT Neigbors (Edge does NOT exist)";
+//        }
+//
+//        break;
+//
+//    case 8:
+//        cout << "Print All Neigbors of a Vertex -" << endl;
+//        cout << "Enter ID of Vertex(State) to fetch all Neigbors : ";
+//        cin >> _name;
+//        G1.getAllNeigborsByID(_name);
+//
+//        break;
+//
+//    case 9:
+//        cout << "Print Graph Operation -" << endl;
+//        G1.print();
+//
+//        break;
+//
+//    default:
+//        cout << "Enter Proper Option number " << endl;
+//    }
+//    cout << endl;
+//
+//} while (option != 0);
 
 return 0;
 }
